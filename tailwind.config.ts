db63a4e98cca91e5c1662c6182b6fb0dbccea0f3
codemodel6 <codemodel6@gmail.com>
2024-09-25 12:51:10 +0900

    design : tailwind 사용 color 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,22 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: {
+          light: "##262523", // 밝은 버전
+          origin: "#121212", // 기본 색상
+          dark: "#000000", // 어두운 버전
+        },
         foreground: "var(--foreground)",
+        primary: {
+          100: "#6ee7b7",
+          200: "#a7f3d0",
+          300: "#34d399",
+        },
+        sub: {
+          100: "#ef4444",
+          200: "#fee2e2",
+          300: "#fecaca",
+        },
       },
       fontFamily: {
         shilla: ["Shilla_CultureB-Bold", "sans-serif"],
