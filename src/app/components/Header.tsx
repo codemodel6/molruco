1358d1e84933f7fa2f43ced78364dee28452c112
codemodel6 <codemodel6@gmail.com>
2024-09-25 16:20:53 +0900

    feat : fullPage.js 도입

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,27 @@
 const Header: React.FC = () => {
   return (
-    <header className="fixed w-screen h-24 bg-sub100 z-50 border-b-2 border-primary100 flex justify-between items-center px-8">
+    <header className="fixed w-full h-24 bg-background-dark z-99 flex justify-between items-center px-8">
       <div className="flex justify-between items-center w-2/5 h-full">
-        <button className="flex flex-col justify-center items-center bg-black text-white h-1/2 w-2/5 text-lg">
+        <button className="text-primary-100 flex flex-col justify-center items-center bg-white h-1/2 w-2/5 text-lg">
           molruco
         </button>
-        <div className="flex flex-col justify-center items-center h-full w-3/5 text-xs">
+        <div className="bg-background-dark text-primary-100 flex flex-col justify-center items-center h-full w-3/5 text-xs">
           몰루코 헤더
         </div>
       </div>
       <div className="flex justify-around items-center w-2/5 h-full">
-        <button className="text-base cursor-pointer">Intro</button>
-        <button className="text-base cursor-pointer">Career</button>
-        <button className="text-base cursor-pointer">Project</button>
-        <button className="text-base cursor-pointer">Education</button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Intro
+        </button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Career
+        </button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Project
+        </button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Education
+        </button>
       </div>
     </header>
   );
---
+++ src/types/global.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "fullpage.js" {
+  const fullpage: any;
+  export default fullpage;
+}
