bef38655f678fa94d749410062eac11a36a61ea4
codemodel6 <codemodel6@gmail.com>
2024-09-27 16:15:56 +0900

    폐기

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,29 +1,33 @@
-const Header: React.FC = () => {
+const Header = () => {
   return (
-    <header className="fixed w-full h-24 bg-background-dark z-99 flex justify-between items-center px-8">
-      <div className="flex justify-between items-center w-2/5 h-full">
-        <button className="text-primary-100 flex flex-col justify-center items-center bg-white h-1/2 w-2/5 text-lg">
-          molruco
-        </button>
-        <div className="bg-background-dark text-primary-100 flex flex-col justify-center items-center h-full w-3/5 text-xs">
-          몰루코 헤더
-        </div>
-      </div>
-      <div className="flex justify-around items-center w-2/5 h-full">
-        <button className="text-base cursor-pointer text-primary-100">
-          Intro
-        </button>
-        <button className="text-base cursor-pointer text-primary-100">
-          Career
-        </button>
-        <button className="text-base cursor-pointer text-primary-100">
-          Project
-        </button>
-        <button className="text-base cursor-pointer text-primary-100">
-          Education
-        </button>
-      </div>
+    <header className="w-full h-24 fixed top-0 left-0 z-10 bg-black">
+      {/* style={{ paddingRight: "16px" }} */}
+      {/* <div className="w-full h-24 bg-black"></div> */}
     </header>
+    // <div className="bg-slate-900 fixed w-full h-24 z-99 flex justify-between items-center px-8">
+    //   <div className="flex justify-between items-center w-2/5 h-full">
+    //     <button className="text-primary-100 flex flex-col justify-center items-center bg-white h-1/2 w-2/5 text-lg">
+    //       molruco
+    //     </button>
+    //     <div className="bg-background-dark text-primary-100 flex flex-col justify-center items-center h-full w-3/5 text-xs">
+    //       몰루코 헤더
+    //     </div>
+    //   </div>
+    //   <div className="flex justify-around items-center w-2/5 h-full">
+    //     <button className="text-base cursor-pointer text-primary-100">
+    //       Intro
+    //     </button>
+    //     <button className="text-base cursor-pointer text-primary-100">
+    //       Career
+    //     </button>
+    //     <button className="text-base cursor-pointer text-primary-100">
+    //       Project
+    //     </button>
+    //     <button className="text-base cursor-pointer text-primary-100">
+    //       Education
+    //     </button>
+    //   </div>
+    // </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
 "use client";
 
-import React, { useEffect } from "react";
-import fullpage from "fullpage.js";
-import "fullpage.js/dist/fullpage.css";
+import Header from "./components/Header";
 
 const Home = () => {
-  useEffect(() => {
-    new fullpage("#fullpage", {
-      // 옵션 설정
-      autoScrolling: true,
-      scrollHorizontally: true,
-    });
-  }, []);
-
   return (
-    <div id="fullpage">
-      <div className="section">First Section</div>
-      <div className="section">Second Section</div>
-      <div className="section">Third Section</div>
-      <div className="section">Fourth Section</div>
+    <div className="relative w-full h-screen overflow-y-scroll">
+      <Header />
+      <div className="w-full bg-primary-100" style={{ height: "2000px" }}></div>
     </div>
+
+    // <div className="relative w-full h-screen snap-y snap-mandatory overflow-scroll">
+    //   <Header />
+    //   <div
+    //     className="snap-y snap-mandatory overflow-scroll"
+    //     style={{ height: "calc(100vh - 6rem)" }}
+    //   >
+    //     <section className="h-screen snap-start bg-blue-500 flex items-center justify-center">
+    //       <h1 className="text-white text-4xl">First Screen</h1>
+    //     </section>
+    //     <section className="h-screen snap-start bg-green-500 flex items-center justify-center">
+    //       <h1 className="text-white text-4xl">Second Screen</h1>
+    //     </section>
+    //     <section className="h-screen snap-start bg-red-500 flex items-center justify-center">
+    //       <h1 className="text-white text-4xl">Third Screen</h1>
+    //     </section>
+    //     <section className="h-screen snap-start bg-yellow-500 flex items-center justify-center">
+    //       <h1 className="text-white text-4xl">Fourth Screen</h1>
+    //     </section>
+    //   </div>
+    // </div>
   );
 };
 
